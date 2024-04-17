c63648aba9ee933da9f2c5d7659e6cb63dfcacfe
Dunn <wg0121@outlook.com>
2024-04-17 23:03:38 +0800

    chore: 调整项目结构和构建方式

---
+++ eng/build.js
@@ -0,0 +1,14 @@
+const esbuild = require("esbuild");
+const path = require("node:path");
+
+const rootPath = path.dirname(__dirname);
+
+esbuild.build({
+  platform: "node",
+  entryPoints: [path.resolve(rootPath, "src/app.js")],
+  outfile: path.resolve(rootPath, "index.js"),
+  bundle: true,
+  packages: "external",
+  target: "node20",
+  minify: true,
+});
