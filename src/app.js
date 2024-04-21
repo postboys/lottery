51afb885ff889a55321c7d2c754d6b0e15482c00
Dunn <wg0121@outlook.com>
2024-04-21 22:59:07 +0800

    feat: 程序启动时立即执行一次同步

--- src/app.js
+++ src/app.js
@@ -4,10 +4,9 @@ const dlt = require("./dlt");
 const dltJob = new CronJob("*/5 * * * *", async () => {
   const success = await dlt.sync();
 
-  // 执行成功，或到了第二天就停止同步
   if (success) {
     dltJob.stop();
   }
-});
+}, null, null, null, null, true);
 
 dltJob.start();
