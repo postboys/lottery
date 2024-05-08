e5f231f17d2d0054599cb605b6d54883f78939dc
Dunn <wg0121@outlook.com>
2024-05-08 11:44:00 +0800

    feat: 更新eslint

--- eng/eslint.config.js
+++ eng/eslint.config.js
@@ -1,31 +1,3 @@
-const stylistic = require("@stylistic/eslint-plugin");
-const eslint = require("@eslint/js");
-const globals = require("globals");
+const configs = require("@pikadun/eslint-config/js");
 
-const customized = stylistic.configs.customize({
-    jsx: false,
-    semi: true,
-    quotes: "double",
-    indent: 4,
-});
-
-/** @type {import("eslint").Linter.FlatConfig[]} */
-const config = [
-    {
-        rules: {
-            ...eslint.configs.recommended.rules,
-            ...customized.rules,
-        },
-        plugins: {
-            "@stylistic": stylistic,
-        },
-        languageOptions: {
-            globals: {
-                ...globals.node,
-            },
-        },
-    },
-
-];
-
-module.exports = config;
+module.exports = configs;
