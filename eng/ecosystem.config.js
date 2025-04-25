065011486d19ed8644aedfa4b9342142c7c547c6
Dunn <wg0121@outlook.com>
2025-04-26 01:06:58 +0800

    chore: use github action (#2)

---
+++ eng/ecosystem.config.js
@@ -0,0 +1,21 @@
+const SECRET_ENV_VARS = JSON.parse(process.env.SECRETS ?? null);
+const APP_ENV = process.env.APP_ENV ?? "lottery-staging";
+
+module.exports = {
+    apps: [
+        {
+            name: APP_ENV,
+            script: "lib/main.js",
+            env: {
+                ...SECRET_ENV_VARS,
+
+                DATABASE_NAME: APP_ENV,
+                DATABASE_USER: APP_ENV,
+            },
+
+            cron_restart: "00 22 * * 1,3,6",
+            stop_exit_codes: [0],
+            time: true,
+        },
+    ],
+};
