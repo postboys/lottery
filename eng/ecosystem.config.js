c4caeba5319249e124f5b3bcb387ea365ed8493a
Dunn <wg0121@outlook.com>
2025-04-26 00:19:48 +0800

    feat: add secrets management to ecosystem configuration

--- eng/ecosystem.config.js
+++ eng/ecosystem.config.js
@@ -1,8 +1,11 @@
+const secrets = JSON.parse(process.env.SECRETS ?? null);
+
 module.exports = {
     apps: [
         {
             name: process.env.APP_ENV,
             script: "lib/main.js",
+            env: { ...secrets },
 
             cron_restart: "00 22 * * 1,3,6",
             stop_exit_codes: [0],
