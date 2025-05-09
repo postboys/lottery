8fa429833ad3e5eb8b12424067a0495449fa68ec
Dunn <wg0121@outlook.com>
2025-05-09 13:55:42 +0800

    chore: remove cron_restart and stop_exit_codes from ecosystem config

--- eng/ecosystem.config.js
+++ eng/ecosystem.config.js
@@ -15,8 +15,6 @@ module.exports = {
                 ...SECRET_ENV_VARS,
             },
 
-            cron_restart: "00 22 * * 1,3,6",
-            stop_exit_codes: [0],
             time: true,
         },
     ],
